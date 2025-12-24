3fa80521bc45d4c19efb81ff7516ba6dd0d9b144
Jack Perry <jack.perry@me.com>
2025-12-24 22:33:55 +1100

    Staged

--- components/standing_desk_height/uplift_decoder.h
+++ components/standing_desk_height/uplift_decoder.h
@@ -2,7 +2,6 @@
 
 #include <stdint.h>
 #include "decoder.h"
-#include "esphome/core/log.h"
 
 namespace esphome {
 namespace standing_desk_height {
@@ -16,29 +15,13 @@ class UpliftDecoder : public Decoder {
       HEIGHT2,  // waiting for second height arg
     } state_ = SYNC1;
     uint8_t buf_[2];
-    
+
   public:
     UpliftDecoder() { }
     ~UpliftDecoder() { }
 
     bool put(uint8_t b);
     float decode();
-};
-
-struct Util {
-  static unsigned int getword(unsigned char a, unsigned char b) {
-    return (static_cast<unsigned>(a) << 8) + b;
-  }
-
-  static unsigned to_mm(unsigned h) {
-    if (h < 600) {
-      // Height in inches*10; convert to mm
-      h *= 254; // convert to mm*100
-      h += 50;  // round up to nearest whole mm
-      h /= 100; // convert to mm
-    }
-    return h;
-  }
 };
 
 }
